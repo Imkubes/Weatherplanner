bef7ffb8d81d333bfde482bc0cccb5978890db22
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-07 09:57:37 -0500

    beginning js

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,28 @@
 var APIKey = "36b80f9a86355f67ae521926147037ad";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=eagan&appid=" + APIKey + "&units=imperial"
+var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=44.8041&lon=-93.1669&appid=" + APIKey + "&units=imperial"
 var city = $('city');
 var temp = $('temp');
 var humidity = $('humidity');
 var windSpeed = $('windspeed');
 var conditonsIcon = $('conditionsIcon');
 var date = $('date');
+var lat = $('lat');
+var lon = $('lon');
 
 fetch(queryURL)
   .then(function (response) {
       return response.json();
   })
   .then(function (data) {
-      searchData = data;
+      //searchData = data;
       console.log(data);
+      addToPage(data);
   })
 
 
   function addToPage(data) {
-      var temp = data.main.temp;
-      console.log(temp);
+      var temp = data.current.temp
+      var humidity = data.current.humidity
+      var windSpeed = data.current.wind_speed
+      console.log(windSpeed);
   }
-
-  addToPage();

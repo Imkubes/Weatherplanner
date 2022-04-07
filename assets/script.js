c7256bdf0cd9bdbe592edc0c9d912203b27ac54f
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-06 22:59:39 -0500

    confused trying to work with api

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,25 @@
 var APIKey = "36b80f9a86355f67ae521926147037ad";
-var city;
-var state;
-var country;
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "," + country + "&appid=" + APIKey
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=eagan&appid=" + APIKey + "&units=imperial"
+var city = $('city');
+var temp = $('temp');
+var humidity = $('humidity');
+var windSpeed = $('windspeed');
+var conditonsIcon = $('conditionsIcon');
+var date = $('date');
 
 fetch(queryURL)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (data) {
+      searchData = data;
+      console.log(data);
+  })
+
+
+  function addToPage(data) {
+      var temp = data.main.temp;
+      console.log(temp);
+  }
+
+  addToPage();

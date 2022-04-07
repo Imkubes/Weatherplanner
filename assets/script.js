210d9acbd68f9069c18a6b32cae561b4ff78ec76
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-07 15:03:11 -0500

    functionality of the current weather forecast

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 var APIKey = "36b80f9a86355f67ae521926147037ad";
-var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=eagan&appid=" + APIKey
+var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=duluth&appid=" + APIKey
 var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=44.8041&lon=-93.1669&appid=" + APIKey + "&units=imperial"
-var city = $('city');
-var temp = $('temp');
-var humidity = $('humidity');
-var windSpeed = $('windspeed');
-var conditonsIcon = $('conditionsIcon');
-var date = $('date');
-var uvi = $('uvi');
-var lat = $('lat');
-var lon = $('lon');
+var city = $('#city');
+var temp = $('#temp');
+var humidity = $('#humidity');
+var windSpeed = $('#windspeed');
+var conditonsIcon = $('#conditionsIcon');
+var date = $('#date');
+var uvi = $('#uvi');
+var lat = $('#lat');
+var lon = $('#lon');
 
 
 fetch(queryURL2)
@@ -24,8 +24,6 @@ fetch(queryURL2)
   function getLatLon(data) {
       var lat = data.coord.lat
       var lon = data.coord.lon
-      console.log(lat);
-      console.log(lon);
   }
 
 
@@ -34,21 +32,22 @@ fetch(queryURL)
       return response.json();
   })
   .then(function (data) {
-      //searchData = data;
       console.log(data);
       addToPage(data);
   })
 
 
   function addToPage(data) {
-      var temp = data.current.temp
-      var humidity = data.current.humidity
-      var windSpeed = data.current.wind_speed
-      var uvi = data.current.uvi
+      var cTemp = data.current.temp
+      var cHumidity = data.current.humidity
+      var cWindSpeed = data.current.wind_speed
+      var cUvi = data.current.uvi
       // Changing date to a readable format
-      var unixDate = data.current.dt
-      var milliseconds = unixDate * 1000
+      var CUnixDate = data.current.dt
+      var milliseconds = CUnixDate * 1000
       var dateObject = new Date(milliseconds)
       var humanDate = dateObject.toLocaleString("en-us", {timeZoneName: "short"})
-      console.log(uvi);
+      console.log(humanDate);
+
+      temp.text('temp: ' + cTemp + '\u00b0');
   }

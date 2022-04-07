f88d481cb835e042e2469092d1876069877422dc
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-07 10:25:57 -0500

    getting lat and lon

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var APIKey = "36b80f9a86355f67ae521926147037ad";
+var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=eagan&appid=" + APIKey
 var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=44.8041&lon=-93.1669&appid=" + APIKey + "&units=imperial"
 var city = $('city');
 var temp = $('temp');
@@ -6,8 +7,27 @@ var humidity = $('humidity');
 var windSpeed = $('windspeed');
 var conditonsIcon = $('conditionsIcon');
 var date = $('date');
+var uvi = $('uvi');
 var lat = $('lat');
 var lon = $('lon');
+
+
+fetch(queryURL2)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (data) {
+      console.log(data);
+      getLatLon(data);
+  })
+
+  function getLatLon(data) {
+      var lat = data.coord.lat
+      var lon = data.coord.lon
+      console.log(lat);
+      console.log(lon);
+  }
+
 
 fetch(queryURL)
   .then(function (response) {
@@ -24,9 +44,11 @@ fetch(queryURL)
       var temp = data.current.temp
       var humidity = data.current.humidity
       var windSpeed = data.current.wind_speed
+      var uvi = data.current.uvi
+      // Changing date to a readable format
       var unixDate = data.current.dt
       var milliseconds = unixDate * 1000
       var dateObject = new Date(milliseconds)
       var humanDate = dateObject.toLocaleString("en-us", {timeZoneName: "short"})
-      console.log(humanDate);
+      console.log(uvi);
   }
